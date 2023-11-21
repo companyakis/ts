9bf7683fbebb75650fa1a3238966d1eb41b4dfbf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-21 08:48:03 +0300

    Create 02-type-annotation.ts

---
+++ 02-type-annotation.ts
@@ -0,0 +1,9 @@
+//introduction to type annotation
+
+let myName: string = "Mustafa";
+
+let year: number = 2023;
+
+let isImportant: boolean = true;
+
+export {}; //Cannot redeclare block-scoped variable 'myName'
