c7e59ccb6fd6db359188b8cc38bd2f8fb274e414
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-21 18:44:45 +0300

    Create 03-arrays.ts

---
+++ 03-arrays.ts
@@ -0,0 +1,18 @@
+let people: string[] = ["MB", "MAB", "VMB", "AOB", "PB"]
+console.log(people)
+
+let numbers: Array<number> = [11, 2, 4, -1]
+console.log(numbers)
+
+let info: (string | number)[] = ["web3", 2023]
+console.log(info)
+
+let values: Array<string | boolean> = [true, "okey"]
+console.log(values)
+
+let subjects: Array<string>;
+subjects = ["finance", "math", "ai"]
+
+for (let i = 0; i < subjects.length; i++) {
+    console.log(subjects[i])
+}
