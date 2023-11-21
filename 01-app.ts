a7152f06df02478f859ebe9040e985449730314e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-21 08:34:14 +0300

    Create 01-app.ts

---
+++ 01-app.ts
@@ -0,0 +1,19 @@
+/* 
+terminal commands:
+1 npm --version typescript
+2 npm install --save-dev lite-server
+
+open package.json and add:
+
+"scripts": {
+    "start": "lite-server"
+}
+
+terminal commands:
+3 npm start
+
+new terminal command:
+1 tsc app.ts -w
+*/
+
+console.log("TypeScript is important for smart contract development!")
