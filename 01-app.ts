52df54403e2f44e52ffa1d1fb5e262b2ea38849a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-21 10:21:43 +0300

    Update 01-app.ts

--- 01-app.ts
+++ 01-app.ts
@@ -1,17 +1,9 @@
 /* 
-terminal commands:
-1 npm --version typescript
+npm --version typescript
 
-server info:
-https://www.npmjs.com/package/lite-server
+tsc app.ts -w
 
-new terminal command:
-1 tsc app.ts -w
-
-! we don't have to use lite server
-open terminal and this:
 node app.js
-
 */
 
 console.log("TypeScript is important for smart contract development!")
