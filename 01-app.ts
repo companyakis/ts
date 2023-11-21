249c035843be1e046c6539d947d765575279207a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-21 09:00:29 +0300

    Update 01-app.ts

--- 01-app.ts
+++ 01-app.ts
@@ -1,16 +1,10 @@
 /* 
 terminal commands:
 1 npm --version typescript
-2 npm install --save-dev lite-server
 
-open package.json and add:
+server info:
+https://www.npmjs.com/package/lite-server
 
-"scripts": {
-    "start": "lite-server"
-}
-
-terminal commands:
-3 npm start
 
 new terminal command:
 1 tsc app.ts -w
