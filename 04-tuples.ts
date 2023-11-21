c22a045d7dfefd4661ac991b2eb11fac32136b16
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-21 18:55:29 +0300

    Create 04-tuples.ts

---
+++ 04-tuples.ts
@@ -0,0 +1,11 @@
+let aboutMe: [string, string, number, boolean] = ["mustafa", "buyukdereli", 2023, true]
+console.log(aboutMe)
+
+let info: [boolean, number, string]
+info = [false, 21, "ok"]
+console.log(info)
+
+let managers: [string, string, number][];
+
+managers = [["mustafa", "defi", 1], ["abraham", "hr", 2]]
+console.log(managers)
