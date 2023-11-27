7a04302ef488ac4f8eeafe608dc5ea7a2c9f7697
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-27 12:15:28 +0300

    Create 07-union.ts

---
+++ 07-union.ts
@@ -0,0 +1,15 @@
+//different types
+
+let info: string | number;
+
+info = "I'm here."
+
+console.log(info)
+
+info = 1923
+
+console.log(info)
+
+//!
+
+//info = true // Type 'boolean' is not assignable to type 'string | number'
