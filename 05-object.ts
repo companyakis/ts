9285b0673c563d0353a0c32973fecdf8309a9e88
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-27 09:00:51 +0300

    Create 05-object.ts

---
+++ 05-object.ts
@@ -0,0 +1,20 @@
+type Employee = {
+    firstName: string,
+    lastName: string,
+    department: string,
+    title: string,
+    age: number
+}
+
+
+let person = {
+    firstName: "Mustafa",
+    lastName: "Buyukdereli",
+    department: "FinTech",
+    title: "Turkiye",
+    age: 29
+}
+
+console.log(person)
+console.log(person.department)
+console.log(person.lastName)
