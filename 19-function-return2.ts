856bf03a285ac8fc1e9efe0e9f01dc1c63258358
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-27 21:06:17 +0300

    Create 19-function-return2.ts

---
+++ 19-function-return2.ts
@@ -0,0 +1,7 @@
+function sayHi(name: string, surname: string) : string {
+    return `Hi ${name} ${surname}`
+}
+
+let greeting = sayHi("Mustafa", "Buyukdereli")
+
+console.log(greeting) //Hi Mustafa Buyukdereli
