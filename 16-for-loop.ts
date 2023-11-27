6899c2a998eabec63d044b4ed6c60bc8d2b611ef
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-27 19:46:52 +0300

    Create 16-for-loop.ts

---
+++ 16-for-loop.ts
@@ -0,0 +1,15 @@
+for (let i = 0; i < 5; i++) {
+    console.log(`i value: ${i}`);
+}
+
+let numArray = [10, 11, 12, 13]
+
+for (let item of numArray) {
+    console.log(item)
+}
+
+let myName = "Mustafa"
+
+for (let item of myName) {
+    console.log(item)
+}
