4af82c951ebacbe5ef834ad658d9c5a2094b9fad
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-27 12:23:14 +0300

    Create 08-any.ts

---
+++ 08-any.ts
@@ -0,0 +1,22 @@
+let info: any
+
+info = "Mustafa"
+
+info = true
+
+info = 2023
+
+console.log(info)
+
+info = {
+    firstName: "Mustafa",
+    lastName : "Buyukdereli"
+}
+
+console.log(info.lastName)
+
+let infoArray: any[]
+
+infoArray = ["Mustafa", true, 2023]
+
+console.log(infoArray)
