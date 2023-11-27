184633ba18774abfb2aea5ccdcb8009076f17905
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-27 19:36:17 +0300

    Create 15-switch.ts

---
+++ 15-switch.ts
@@ -0,0 +1,22 @@
+let personId = 13
+
+switch (personId) {
+    case 1:
+        console.log("Mustafa")
+        break
+    case 2:
+        console.log("Bilge")
+        break
+    case 3:
+        console.log("Kültigin")
+        break
+    case 4:
+        console.log("Attila")
+        break
+    case 5:
+        console.log("Mete")
+        break
+    default:
+        console.log("Undefined person!")
+        break
+}
